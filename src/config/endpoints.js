46472f6c136301745aa1d5bb426542eddf255b86
JuP <julien.pierre.picard@gmail.com>
2020-11-01 17:43:11 +0100

    change endpoints

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -4,7 +4,7 @@ const endpoints = {
         BASE_API: "http://localhost:4000" //"http://192.168.1.10:4100"
     },
     production: {
-        BASE_URL: "http://olivier-picard.com",
+        BASE_URL: "https://olivier-picard.com",
         BASE_API: "https://api.olivier-picard.com" //"https://flickr-gallery-api.herokuapp.com"
     }
 }[process.env.NODE_ENV]
